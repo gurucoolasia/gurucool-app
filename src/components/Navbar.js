1e39beecc981987bfa042f4f7f95565d670d138f
SkandTiwari <tiwariskand11@gmail.com>
2021-09-12 21:10:52 +0530

    commit for logo and button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
 import './navbar.css'
+
+
+
 function Navbar() {
+
+
     return (
         <>
            <div className = 'main'>
                <div className = 'logo-space'>
                    logo
                </div>
+
                <div className = 'nav-list'>
                    <ul className = "nav-item-list">
                        <li><a href="#">Learn More</a></li>
@@ -16,6 +21,9 @@ function Navbar() {
                        <li><a href="#">Getting Started</a></li>
                    </ul>
                </div>
+               <div className = "toggle-collapse">
+                   <span><i class="fa fa-bars"></i></span>
+                </div>
                <div className = 'logout-space'>
                     logout
                </div>
